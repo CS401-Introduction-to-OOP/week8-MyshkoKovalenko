240becf3bd3e6739bb930968cef6ab2a842dd5dc
Mykhailo Kovalenko <mykhaylo2007@gmail.com>
2026-03-24 18:43:39 +0200

    Part 2 complete

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,17 @@
+class CargoContainer<T> where T : DeliveryItem
+{
+    List<T> _items;
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        double cost = 0;
+        foreach(T i in _items)
+            cost += i.CalculateCost();
+        return cost;
+    }
+}
